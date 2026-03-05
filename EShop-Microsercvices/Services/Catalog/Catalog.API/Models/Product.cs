793c70b26d61bfdb4503e56288affbc5fa40874a
Rahul <rahul999.kumar@gmail.com>
2026-03-06 00:13:55 +0530

    Save My First Command.

---
+++ EShop-Microsercvices/Services/Catalog/Catalog.API/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Catalog.API.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public List<string> Categories { get; set; } = new();
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; } = default!;
+    }
+}
