3445c9b1d5c4ca02510b8a1e103ae9d199d1600f
Rahul <rahul999.kumar@gmail.com>
2026-03-06 23:30:28 +0530

    Added Building Blocks

---
+++ EShop-Microsercvices/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, string Description, List<string> Categories, decimal Price, string ImageUrl): IRequest<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
